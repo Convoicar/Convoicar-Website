c70f21c492467c4c56010a28551aa754ab867e9d
JB Dubois <jeanbaptiste@re-ve.fr>
2020-09-12 06:20:15 +0200

    v01

--- src/assets/js/cloudinaryjb.js
+++ src/assets/js/cloudinaryjb.js
@@ -3,17 +3,45 @@
 
 (function() {
 
+  var cl = new cloudinary.Cloudinary({cloud_name: "des7ayfwu", secure: true});
+  var audi = cl.imageTag('logos/audi', {effect: "colorize", color: "white", height: 12, quality: 100, crop: "scale", format: "png"}).toHtml();
+
 if (document.getElementById("img-band1")!==null) {
-
-    var cl = new cloudinary.Cloudinary({cloud_name: "des7ayfwu", secure: true});
-  // insertion de mes images au fur et à mesure
-    var audi = cl.imageTag('logos/audi', {effect: "colorize", color: "#293462", height: 12, quality: 100, crop: "scale", format: "png"}).toHtml();
-    // var getaround = cl.imageTag('logos/getaround', {effect: "colorize", color: "#293462", height: 30, quality: 100, crop: "scale", format: "png"}).toHtml();
-    // var ada = cl.imageTag('logos/ada2', {effect: "colorize", color: "#293462", height: 20, quality: 100, crop: "scale", format: "png"}).toHtml();
-    // var enterprise = cl.imageTag('logos/enterprise2', {transformation: [
-    //   {effect: "make_transparent:66"},{effect: "colorize", color: "#293462", height: 30, quality: 100, crop: "scale", format: "png"}]}).toHtml();
-
-    document.getElementById("img-band1").innerHTML = audi;
+document.getElementById("img-band1").innerHTML = audi;
+};
+if (document.getElementById("img-band2")!==null) {
+document.getElementById("img-band2").innerHTML = audi;
+};
+if (document.getElementById("img-band3")!==null) {
+document.getElementById("img-band3").innerHTML = audi;
+};
+if (document.getElementById("img-band4")!==null) {
+document.getElementById("img-band4").innerHTML = audi;
+};
+if (document.getElementById("img-band5")!==null) {
+document.getElementById("img-band5").innerHTML = audi;
+};
+if (document.getElementById("img-band6")!==null) {
+document.getElementById("img-band6").innerHTML = audi;
+};
+if (document.getElementById("img-band7")!==null) {
+document.getElementById("img-band7").innerHTML = audi;
+};
+if (document.getElementById("img-band8")!==null) {
+document.getElementById("img-band8").innerHTML = audi;
+};
+if (document.getElementById("img-band9")!==null) {
+document.getElementById("img-band9").innerHTML = audi;
+};
+if (document.getElementById("img-band10")!==null) {
+  document.getElementById("img-band10").innerHTML = audi;
   };
+  if (document.getElementById("img-band11")!==null) {
+  document.getElementById("img-band11").innerHTML = audi;
+  };
+  if (document.getElementById("img-band12")!==null) {
+  document.getElementById("img-band12").innerHTML = audi;
+  };
+
 
 })();
